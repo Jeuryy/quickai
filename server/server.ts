2e64dcab4ee73dac274a83119d4bcc4a3ce82a40
Jeury Pierre Dide <elyruej.102004@gmail.com>
2025-12-03 15:15:20 -0400

    Reestructuring files

---
+++ server/server.ts
@@ -0,0 +1,24 @@
+import express from "express"
+import  cors from "cors"
+import "dotenv/config"
+import { clerkMiddleware, requireAuth } from '@clerk/express'
+import aiRouter from "./routes/aiRoutes.ts"
+
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(clerkMiddleware())
+
+app.get('/', (req, res) => res.send('Server is live!'))
+
+app.use(requireAuth())
+
+app.use('/api/ai', aiRouter)
+
+const PORT = process.env.PORT || 5173;
+
+app.listen(PORT, () => {
+    console.log("Server is running on port", PORT);
+})
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+"use client"
 import AiTools from "@/components/AiTools";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Plan from "@/components/Plan";
 import Testimonial from "@/components/Testimonials";
+import { useAuth } from "@clerk/nextjs";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  const {getToken} = useAuth();
+  useEffect(()=> {
+    getToken().then((token)=>console.log(token));
+  }, [])
+
   return (
     <>
         <Navbar/>
