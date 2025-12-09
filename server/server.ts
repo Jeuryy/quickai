4f9a92f5c3d6884dba9520040412b748043a48fb
Jeury Pierre Dide <elyruej.102004@gmail.com>
2025-12-09 14:22:53 -0400

    .ts

--- server/server.ts
+++ server/server.ts
@@ -2,9 +2,9 @@ import express from "express"
 import  cors from "cors"
 import "dotenv/config"
 import { clerkMiddleware, requireAuth } from '@clerk/express'
-import aiRouter from "./routes/aiRoutes.ts"
-import connectCloudinary from "./configs/cloudinary.ts"
-import userRouter from "./routes/userRoutes.ts"
+import aiRouter from "./routes/aiRoutes.js"
+import connectCloudinary from "./configs/cloudinary.js"
+import userRouter from "./routes/userRoutes.js"
 
 
 const app = express()
