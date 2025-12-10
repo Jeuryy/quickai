161616ca575a01ee1875ce35815a68d20213bd9b
Jeury Pierre Dide <elyruej.102004@gmail.com>
2025-12-09 21:20:45 -0400

    adding z-index to sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -27,7 +27,7 @@ const Sidebar = ({sidebar, setSidebar}: Props) => {
     const {data: subscription} = useSubscription()
 
   return (
-    <div className={`w-60 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-14 bottom-0
+    <div className={`w-60 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-14 bottom-0 z-99
     ${sidebar ? 'translate-x-0' : 
     'max-sm:-translate-x-full'} transition-all duration-300 ease-in-out`}>
         <div className='my-7 w-full'>
